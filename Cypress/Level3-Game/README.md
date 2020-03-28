d819b4ca076833e2d0be6261270cfc0ae85df5e3
Leo Chelliah <leopraveen.c@gmail.com>
2020-03-29 05:19:02 +0530

    Level3-Game starts

---
+++ Cypress/Level3-Game/README.md
@@ -0,0 +1,6 @@
+# Level1-Game 
+
+In this example files, 
+   1. Configure a web application
+   2. Configure npm with express & cypress
+   3. Build and Run
---
+++ Cypress/Level3-Game/index.js
@@ -0,0 +1,28 @@
+
+const express = require("express");
+const path = require("path");
+const app = new express();
+const PORT = 3000;
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+
+});
+
+app.post("/game", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "game.html"));
+
+});
+
+app.post("/logout", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "logout.html"));
+
+});
+
+app.post("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
