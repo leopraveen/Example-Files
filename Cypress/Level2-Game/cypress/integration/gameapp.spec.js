febde7ca2323f6a9acc67e188f39420f38940a9f
Leo Chelliah <hr@dreamguys.co.in>
2020-04-01 14:28:57 +0100

    Updates

--- Cypress/Level2-Game/cypress/integration/gameapp.spec.js
+++ Cypress/Level2-Game/cypress/integration/gameapp.spec.js
@@ -1,37 +1,16 @@
-describe("UI Tests", () => {
+    describe("UI Tests", () => {
 
-    it("Load the login page correctly", () => {
-        
-        cy.visit("http://localhost:3000");
-        cy.get('[data-cy=login-text]').should("have.length", 1)
-        cy.get('[data-cy=login-text]').should("be.visible")
-    })
+	it("Load the login page correctly", () => {
 
-    it("Check login doesnt work without username", () => {
         cy.visit("http://localhost:3000");
-        cy.get('[data-cy=Password]').type("123456");
-        cy.get('[data-cy=Button]').click;
-        // Check if we are still in the login page
-        cy.get('[data-cy=login-text]').should("have.length", 1);
-        cy.get('[data-cy=HomeGame]').should("have.length",0);
-    })
+        cy.get('[data-cy=login-text]').should("be.visible" );
+        cy.get('[data-cy=Email]').should("have.length", 1);
+        cy.get('[data-cy=Email2]').should("have.length",0);
 
-    it("Check login doesnt work without password", () => {
-        cy.visit("http://localhost:3000");
-        cy.get('[data-cy=Email]').type("leo@gmail.com");
-        cy.get('[data-cy=Button]').click;
-        // Check if we are still in the login page
-        cy.get('[data-cy=login-text]').should("have.length", 1);
-        cy.get('[data-cy=HomeGame]').should("have.length",0);
-    })
-
-    it("Check login doesnt work without password", () => {
-        cy.visit("http://localhost:3000");
         cy.get('[data-cy=Email]').type("leo@gmail.com");
         cy.get('[data-cy=Password]').type("123456");
         cy.get('[data-cy=Button]').click();
-        // Check if we are still in the login page
-        cy.get('[data-cy=login-text]').should("have.length", 0);
         cy.get('[data-cy=HomeGame]').should("have.length",1);
-    })
+	})
+
 })
